a21681b25b9c4d42e95f24f2d3ec11e3b962a11e
hbery <a.twardosz98@gmail.com>
2024-01-19 01:23:18 +0100

    feat: add database creation to main runner

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,42 @@
-from fastapi import FastAPI
+from logging import Logger
+from os import getenv
+from pathlib import Path
+from sys import exit
 
-from .routers import jobs, schedule, preferences, results
+from database.connection import create_database
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from routers.jobs import job_router
+from routers.preferences import pref_router
+from routers.results import res_router
+from routers.schedule import sched_router
+
+log = Logger(__file__)
+
+dbloc = getenv("DB_LOCATION")
+if dbloc is None:
+    log.error("No DB_LOCATION env variable. Don't know where to find db.")
+    exit(1)
+
+if not Path(dbloc).exists:
+    create_database("schema.dbml")
 
 app = FastAPI()
 
-app.include_router(jobs.router)
-app.include_router(schedule.router)
-app.include_router(preferences.router)
-app.include_router(results.router)
+app.include_router(job_router)
+app.include_router(sched_router)
+app.include_router(pref_router)
+app.include_router(res_router)
+
 
 @app.get("/")
-async def get_base():
-    return {"Hello": "World"}
+async def get_base() -> JSONResponse:
+    return JSONResponse(
+        content=dict(
+            detail=list(
+                dict(
+                    msg="This is ClassDivider backend API. It will solve your problem."
+                )
+            )
+        )
+    )
