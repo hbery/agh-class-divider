162cdbe89146b7ea9d2f69d6f019bb01a731c4da
hbery <a.twardosz98@gmail.com>
2024-01-20 07:23:49 +0100

    feat: update main

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,39 @@
-from logging import Logger
+from logging import INFO, basicConfig, getLogger
 from os import getenv
-from pathlib import Path
 from sys import exit
 
 from database.connection import create_database
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 from fastapi.responses import JSONResponse
 from routers.jobs import job_router
 from routers.preferences import pref_router
 from routers.results import res_router
 from routers.schedule import sched_router
 
-log = Logger(__file__)
+basicConfig(level=INFO)
+log = getLogger(__name__)
 
 dbloc = getenv("DB_LOCATION")
 if dbloc is None:
     log.error("No DB_LOCATION env variable. Don't know where to find db.")
     exit(1)
 
-if not Path(dbloc).exists:
-    create_database("schema.dbml")
+create_database("schema.dbml")
+
+
+def openapi_settings():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="ClassDivider API Spec",
+        version="1.0.0",
+        summary="This is the backend API for ClassDivider application.",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
 
 app = FastAPI()
 
@@ -28,15 +42,15 @@ app.include_router(sched_router)
 app.include_router(pref_router)
 app.include_router(res_router)
 
+app.openapi = openapi_settings
+
 
 @app.get("/")
 async def get_base() -> JSONResponse:
     return JSONResponse(
-        content=dict(
-            detail=list(
-                dict(
-                    msg="This is ClassDivider backend API. It will solve your problem."
-                )
-            )
-        )
+        {
+            "detail": [
+                {"msg": "This is ClassDivider backend API. It will solve your problem."}
+            ]
+        }
     )
